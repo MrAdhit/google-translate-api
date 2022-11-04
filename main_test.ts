045a602b12b6d8bd6b150c22ca8e7e910f900416
MrAdhit <adhityarimba97@gmail.com>
2022-11-04 23:48:34 +0700

    Update main_test.ts

--- main_test.ts
+++ main_test.ts
@@ -1,10 +1,6 @@
 import { assertEquals } from "https://deno.land/std@0.161.0/testing/asserts.ts";
 import { Translator } from "./main.ts";
 
-// Deno.test(function addTest() {
-//   assertEquals(add(2, 3), 5);
-// });
-
 Deno.test(async function test_translate() {  
   assertEquals((await new Translator().translate("Kamu")).text, "You");
 });
